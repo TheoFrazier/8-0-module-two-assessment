5dcf1d561265529df80b16bdcf0e1ca42e8c6b3c
Theodore Frazier <theo.frazier@icloud.com>
2021-10-15 15:11:29 -0400

    solved cannot set innerHTML of null by adding the pound because it's and ID

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 fetch("https://ghibliapi.herokuapp.com/films") //requesting JSON
-.then((res) => {
-    return res.json()
-})
-.then((data) => {
-console.log(data)
-
-const dropDown = document.querySelector("#movie-selection")
-const form = document.querySelector("review-form")
-const userUnput = document.querySelector
-const reviewSubmission = document.querySelector
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
 
 
-
-/* iteration */ 
-
-for (let movie of data) {
+        const dropDown = document.querySelector("#movie-selection")
+        const form = document.querySelector("review-form")
+        const userUnput = document.querySelector
+        const reviewSubmission = document.querySelector
+        
 
 
 
-    const option = document.createElement("option")
-    option.textContent = movie.title;
-    dropDown.append(option)
-}
+        /* iteration */
+
+        for (let movie of data) {
+
+            const option = document.createElement("option")
+            option.textContent = movie.title;
+            dropDown.append(option)
+
+            dropDown.addEventListener("change", (event) => {
+                event.preventDefault();
+                /* we must check if the current selection of the dropdown matches the movie - we must check every movie*/
+
+                for (let currentMovie of data) {
+                    if (dropDown.value === currentMovie.title) {
+                        console.log(currentMovie.title)
+
+                        const displayInfo = document.querySelector("#movie-information")
+                        
+
+                        displayInfo.innerHTML =
+                            `<h2>${currentMovie.title}</h2
+                            <p>${currentMovie.release_date}</p>
+                            <p>${currentMovie.description}>/p>`;
 
 
-}).catch((err) => {
-    console.log(err)
-});
+                    }
+                }
+            })
+        }
+    })
+
+
+            /* Use change not "click" */
+
+
+
+
